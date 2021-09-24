09d58b1860e0d985ebe7c60ef97b85d198d41530
Nehal <patelnehal4u@gmail.com>
2021-09-24 22:25:46 +0530

    initial commit

---
+++ database/migrations/2021_09_24_162450_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->default('text'); //text,image,audio,video
+            $table->unsignedTinyInteger('level')->default(1);
+            $table->string('question');
+            $table->string('option_1');
+            $table->string('option_2');
+            $table->string('option_3');
+            $table->string('option_4');
+            $table->unsignedTinyInteger('correct');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
